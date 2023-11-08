ae2706fb2c627b9fb961c9d5123d378106864b45
BivekHeka <bevillheka@gmail.com>
2023-11-08 17:39:47 +0545

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const color = require("colors");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+//dotenv config
+dotenv.config();
+require('dotenv').config();
+
+//mongodb connection
+connectDB();
+
+//rest object
+const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(morgan("dev"));
+
+//routes
+app.use("/api/v1/user", require("./routes/userRoutes"));
+app.use("/api/v1/admin", require("./routes/adminRoutes"));
+app.use("/api/v1/doctor", require("./routes/doctorRoutes"));
+
+//port
+const port = process.env.PORT || 8080
+//listen port
+
+app.listen(port, () => {
+    console.log(`Server Running in ${process.env.DEV_MODE} Mode on port ${port}`.bgCyan.white);
+});
